6c2190bbc1978e2ab7e27630a5707a36b3e913ec
Muhammed Fatih Balin <m.f.balin@gmail.com>
2021-10-21 12:24:41 -0400

    Update

---
+++ docs/precache-manifest.09cb0bc3f32a12da60ea3531cd747159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defec3d85dba30f01ebef3f0a6f48440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03fdf84cc70cfdd0b927",
+    "url": "/static/css/main.d67d397a.chunk.css"
+  },
+  {
+    "revision": "24a0f3484701c702e304",
+    "url": "/static/js/2.c1483f95.chunk.js"
+  },
+  {
+    "revision": "03fdf84cc70cfdd0b927",
+    "url": "/static/js/main.d03c2ce3.chunk.js"
+  },
+  {
+    "revision": "9bc960cf1d1c6c165276",
+    "url": "/static/js/runtime-main.fa229ef1.js"
+  }
+]);
