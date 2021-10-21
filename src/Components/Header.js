1da7d30445ce32b430845ae392deb02d8a32508d
Muhammed Fatih Balin <m.f.balin@gmail.com>
2021-10-21 12:19:22 -0400

    Update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -49,11 +49,12 @@ class Header extends Component {
                 Works
               </a>
             </li>
-            /*<li>
+            <!---
+            <li>
               <a className="smoothscroll" href="#testimonials">
                 Testimonials
               </a>
-            </li>*/
+            </li> --->
             <li>
               <a className="smoothscroll" href="#contact">
                 Contact
