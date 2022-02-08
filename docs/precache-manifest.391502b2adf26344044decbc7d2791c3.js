03281c83fa5f7ac25a577a57147f4827f59d7c76
Muhammed Fatih Balin <m.f.balin@gmail.com>
2022-02-07 21:04:00 -0500

    Update

---
+++ docs/precache-manifest.391502b2adf26344044decbc7d2791c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744ee7253175d592e2c9ce73f0f2a704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e61ea48c3ded7dae3c6",
+    "url": "/static/css/main.00bffb16.chunk.css"
+  },
+  {
+    "revision": "ca8aaf33c2978a07a4bf",
+    "url": "/static/js/2.f1c16cd8.chunk.js"
+  },
+  {
+    "revision": "2e61ea48c3ded7dae3c6",
+    "url": "/static/js/main.985fcf20.chunk.js"
+  },
+  {
+    "revision": "dfc3033a5e0cc76d4639",
+    "url": "/static/js/runtime-main.89b79939.js"
+  }
+]);
--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,7 @@ class About extends Component {
     if (this.props.data) {
       var name = this.props.data.name;
       var profilepic = "images/" + this.props.data.image;
+      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
